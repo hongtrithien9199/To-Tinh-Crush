a7d39d202a8e3a8d6265c4951783f002c3212123
Hồng trí Thiện <90546872+hongtrithien9199@users.noreply.github.com>
2021-09-12 14:46:35 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 Bịconst CONFIG = {
-    introTitle: ' Gửi; Huỳnh Anh',
+    introTitle: 'huynh anh',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
